962b3848eca365a063cf79afad477472ceafec9b
seyithan.topal <topal@sigo.green>
2021-12-06 23:50:19 +0100

    Installed dotenv package to use .env file so that API key could be stored safely and refactored the code

--- controllers/artist.js
+++ controllers/artist.js
@@ -1,7 +1,18 @@
 const axios = require('axios');
 
 exports.getArtistsByName = async (req, res) => {
-  const { name } = req.params;
-  const { data : { results: { artistmatches: artists } } } = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=5f58053a70355a15c3bc170df9146d3e&format=json`)
-  return res.status(200).json({ message: 'OK', artists });
+  try {
+    const { name } = req.params;
+    const params = {
+      method: 'artist.search',
+      artist: name,
+      api_key: process.env.API_KEY,
+      format: 'json',
+    };
+    console.log('process.env.API_KEY: ', process.env.API_KEY);
+    const { data : { results: { artistmatches: artists } } } = await axios.get(`http://ws.audioscrobbler.com/2.0/?`, { params });
+    return res.status(200).json({ message: 'OK', artists });
+  } catch (err) {
+    return res.status(400).json({ message: `Something went wrong during fetching artists: ${err}` });
+  }
 };
