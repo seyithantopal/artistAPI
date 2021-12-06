2be0354557e221193d6219743ce4fa08410f86bc
seyithan.topal <topal@sigo.green>
2021-12-06 23:32:54 +0100

    Installed axios to fetch the artists by making a request to the endpoint mentioned in the test and made a request to get them

--- controllers/artist.js
+++ controllers/artist.js
@@ -1,5 +1,7 @@
-exports.getArtistsByName = (req, res) => {
+const axios = require('axios');
+
+exports.getArtistsByName = async (req, res) => {
   const { name } = req.params;
-  console.log('name: ', name);
-  return res.status(200).json({ message: 'OK' });
+  const { data : { results: { artistmatches: artists } } } = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=5f58053a70355a15c3bc170df9146d3e&format=json`)
+  return res.status(200).json({ message: 'OK', artists });
 };
